44207e45b7c949dc9fbe40e3956482514f5ea5f7
Ryan M. L. McFadden <rmlm@chem.ubc.ca>
2020-11-16 23:57:00 -0800

    update the logo: put more space between the axes and the data/curve & print the site's name

--- scripts/logo.py
+++ scripts/logo.py
@@ -40,8 +40,8 @@ ax.plot(fit["time"], fit["asymmetry"], "-", color="magenta", zorder=3, label="fi
 # ax.set_xlabel("Time (s)", color="black")
 # ax.set_ylabel("Asymmetry", color="black")
 
-ax.set_xlim(None, 13)
-ax.set_ylim(None, fit["asymmetry"][0])
+ax.set_xlim(-0.15 * 13.5, 13.5)
+ax.set_ylim(-0.20 * fit["asymmetry"][0], fit["asymmetry"][0])
 
 # ax.set_title("rmlmcfadden.github.io")
 # fig.suptitle("rmlmcfadden.github.io", color="purple")
@@ -86,5 +86,107 @@ fig.savefig("logo.png", dpi=900, transparent=True)
 # use imagemagick to create the favicon from the rendered image
 cmd = "convert logo.png -background none -resize 128x128 -density 128x128 favicon.ico"
 os.system(cmd)
+
+# plt.show()
+
+fig2, ax2 = plt.subplots(
+    1,
+    2,
+    sharex=True,
+    sharey=True,
+    figsize=(3 * 3.0 * scale, 3.0 * scale),
+    gridspec_kw={"width_ratios": [1, 2]},
+    constrained_layout=True,
+)
+
+ax21, ax22 = ax2
+
+# make the spines thicker
+for axis in ["top", "bottom", "left", "right"]:
+    ax21.spines[axis].set_linewidth(3)
+
+# make the axis spines invisible
+ax21.spines["right"].set_visible(False)
+ax21.spines["top"].set_visible(False)
+
+# turn off the axes labels/ticks
+ax21.tick_params(
+    axis="both",
+    which="both",
+    left=False,
+    bottom=False,
+    right=False,
+    top=False,
+    labelleft=False,
+    labelbottom=False,
+)
+
+ax21.errorbar(
+    data["time"],
+    data["asymmetry"],
+    yerr=data["asymmetry_error"],
+    fmt="o",
+    color="black",
+    # markerfacecolor="lightgrey",
+    zorder=2,
+    label="data",
+)
+
+ax21.plot(fit["time"], fit["asymmetry"], "-", color="magenta", zorder=3, label="fit")
+
+ax21.set_xlim(-0.15 * 13.5, 13.5)
+ax21.set_ylim(-0.20 * fit["asymmetry"][0], fit["asymmetry"][0])
+
+# make the axis completely invisible
+ax22.set_axis_off()
+
+ax22.text(
+    0.0,
+    0.5,
+    "rmlmcfadden\n.github.io",
+    size=23,
+    # weight="bold",
+    ha="left",
+    va="center",
+    transform=ax22.transAxes,
+)
+
+"""
+ax22.text(
+    0.0,
+    1.0,
+    "rmlmcfadden",
+    size=23,
+    color="black",
+    ha="left",
+    va="top",
+    transform=ax22.transAxes,
+)
+
+ax22.text(
+    0.0,
+    0.5,
+    ".github",
+    size=23,
+    color="grey",
+    ha="left",
+    va="center",
+    transform=ax22.transAxes,
+)
+
+ax22.text(
+    0.0,
+    0.0,
+    ".io",
+    size=23,
+    color="grey",
+    ha="left",
+    va="bottom",
+    transform=ax22.transAxes,
+)
+"""
+
+# save the figure as a high resolution transparent png
+fig2.savefig("logo.png", dpi=900, transparent=True)
 
 # plt.show()
