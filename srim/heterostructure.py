3476e6feb1585e24651d5555f2a6f0c98fded59a
Ryan M. L. McFadden <rmlm@chem.ubc.ca>
2020-11-19 12:16:49 -0800

    update the notes on pysrim and the heterostructure target example

--- srim/heterostructure.py
+++ srim/heterostructure.py
@@ -7,41 +7,51 @@ from srim import TRIM, Ion, Layer, Target
 from srim.output import Results, Range
 
 
-# implantation energies (eV)
+# desired implantation energies (eV)
 Energy_eV = 1e3 * np.array([1.0, 3.0, 5.0, 7.0])
 
-# simulated ions
+# number of simulated ions
 N_Ions = 1e3
 
-# mass of 8Li (amu)
+# mass of the implanted ion 8Li (amu)
 m_8Li = 8.02248625
 
-# Specify the directory of SRIM.exe
-# For windows users the path will include C://...
-srim_executable_directory = "/home/rmlm/software/srim-2013"
+# specify the directory of SRIM.exe
+# for Windows users the path will be of the form "C://..."
+srim_executable_directory = "/path/to/srim-2013"
 
-# create a heterostructure consisting of alternating layers of LaNiO3 and LaAlO3
+# create a heterostructure consisting of n_repetitions of alternating layers of
+# the perovskites LaNiO3 and LaAlO3
+# these have the (approximate) cubic lattice constants:
+a_LaNiO3 = 3.857
+a_LaAlO3 = 3.787
+
+# use an empty list to hold each layer
 layers = []
-for i in range(3):
-    # 2 u.c. lanthanum nickelate layer
+
+# number of layer repetitions
+n_repetions = 3
+
+# loop over the repetions to create the heterostructure!
+for i in range(n_repetions):
+    # 2 unit cell thick lanthanum nickelate layer
     LaNiO3 = Layer(
         {"La": {"stoich": 1.0}, "Ni": {"stoich": 1.0}, "O": {"stoich": 3.0},},
         density=7.11,
-        width=2 * 3.857,
+        width=2 * a_LaNiO3,
         name="LaNiO3(%d)" % i,
     )
     layers.append(LaNiO3)
-
-    # 4 u.c. lanthanum aluminate layer
+    # 5 unit cell thick lanthanum aluminate layer
     LaAlO3 = Layer(
         {"La": {"stoich": 1.0}, "Al": {"stoich": 1.0}, "O": {"stoich": 3.0},},
         density=6.52,
-        width=4 * 3.787,
+        width=5 * a_LaAlO3,
         name="LaAlO3(%d)" % i,
     )
     layers.append(LaAlO3)
 
-# 0.5 mm sapphire substrate
+# use a 0.5 mm sapphire layer as the bottom "substrate"
 Al2O3 = Layer(
     {"Al": {"stoich": 2.0}, "O": {"stoich": 3.0},},
     density=3.95,
@@ -49,17 +59,15 @@ Al2O3 = Layer(
     name="Al2O3",
 )
 layers.append(Al2O3)
-# print(layers)
+
+# construct the mulitlayer target using the layers list
+target = Target(layers)
 
 # loop over implantation energies
 for E_eV in Energy_eV:
-    # Construct a the implanted 8Li ion
+    # Construct the implanted 8Li ion
     ion = Ion("Li", energy=E_eV, mass=m_8Li)
-
-    # Construct a target of a single layer of Nickel
-    target = Target(layers)
-
-    # Initialize a TRIM calculation with given target and ion
+    # Initialize a TRIM calculation with the ion & target
     trim = TRIM(
         target,
         ion,
@@ -79,11 +87,12 @@ for E_eV in Energy_eV:
         angle_ions=0,
     )
 
-    # run the trim calculation (my take a few seconds to start)
-    # if all went well, a TRIM window will pop up!
+    # run the TRIM calculation
+    # - this may take a few seconds to start (especially using wine)
+    # - if all went well, a TRIM window will pop up!
     results = trim.run(srim_executable_directory)
 
-    # move all the results to a new directory
-    output_directory = "results/heterostructure/%.0f_eV" % E_eV
+    # move all the results for each implantation energy to a new directory
+    output_directory = "results/%.0f_eV" % E_eV
     os.makedirs(output_directory, exist_ok=True)
     TRIM.copy_output_files(srim_executable_directory, output_directory)
--- srim/pysrim.md
+++ srim/pysrim.md
@@ -22,12 +22,12 @@ calculations. Its key strength is it provides a convenient interface to
 programatically define the calculation input. This is particularly
 advantageous as it:
 
-- allows for easy batch calculations where a few sim params are systematically
-  varied (e.g., the ion implantation energy).
-- enables the use of `for` loops to create multilayered heterostructures, which
-  is painful to do otherwise.
+- allows for easy "batch" calculations where a few simulation parameters are
+  systematically varied (e.g., the ion implantation energy).
+- allows the use of `for` loops when creat multilayered heterostructures, which
+  is arduous otherwise.
 - makes the record of the calculation <i>much</i> more readable than `SRIM`'s
-  own custom input format.
+  own custom (and dated) input format.
 
 It even works well when running on Linux
 (i.e., it automatically calls `wine SRIM.exe`, `wine TRIM.exe`, etc.)!
@@ -38,30 +38,41 @@ It even works well when running on Linux
 `pysrim`, unfortunately, isn't perfect and I ran into the following issues while
 using it:
 
-- Everytime it runs, it complains that the `.yaml` file its reading isn't do  ne
+- Everytime it runs, it complains that the `.yaml` file its reading isn't done
   safely. This is due to a recent change in `pyYAML` requiring a `Loader` to be
   specified as a `kwarg`. There is currently a
   [merge request](https://gitlab.com/costrouc/pysrim/-/merge_requests/4) on the
   project's GitLab page to fix this, but until this gets officially patched it
   can be mended by editing line 10 of `srim/core/elemementdb.py` so that it
   reads:
-  {% highlight python %}
-  return yaml.load(open(dbpath, "r"), Loader=yaml.SafeLoader)
-  {% endhighlight %}
+
+{% highlight python %}
+return yaml.load(open(dbpath, "r"), Loader=yaml.SafeLoader)
+{% endhighlight %}
+
 - It fails to parse `IONIZ.txt` output when the ion energy is less than 1 keV.
   The problem is with the regex expression used to parse the output. As a simple
   workaround, rather than fixing the problem, it is sufficient to just comment
   out line 36 of `srim/output.py` so that it reads:
-  {% highlight python %}
-  # raise SRIMOutputParseError("unable to extract ion from file")
-  {% endhighlight %}
+
+{% highlight python %}
+# raise SRIMOutputParseError("unable to extract ion from file")
+{% endhighlight %}
+
 - The autosave input in `TRIM` setup does not appear to work; the calculation
   won't run if the `kwarg` is specified. This is relatively minor, so my
   workaround is to just avoid providing the parameter.
 
-## Example
+## Example: a multi-layered heterostructure
 
-### Heterostructure
+Here I give an example showcasing `pysrim`'s capabilities.
+The script below creates a target with a complex layered structure, which would
+be unpleasent to do "by hand", and runs several `TRIM` calculations for a single
+ion at a few different implantation energies. Conveniently, the script takes
+care of moving all the results their own (new) folder after each calculations
+completes.
+
+Try it out yourself!
 
 {% highlight python %}
 {% include_relative heterostructure.py %}
